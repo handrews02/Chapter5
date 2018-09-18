6b36e25940dc2d637f678ab47e74dccb836e465b
Hannah Andrews <handrews02@users.noreply.github.com>
2018-09-18 09:28:12 -0500

    updated text

---
+++ Chapter5/src/Practice/DoorGame.java
@@ -0,0 +1,39 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class DoorGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int doorChoice;
+			Scanner input = new Scanner (System.in);
+			String result;
+			System.out.println("Welcome to the amazing door choice game!");
+			System.out.println("Your host Steve Harvey");
+			System.out.println("Pick a door any door. \n1- for door One\n2- for door Two\n3- for door Three");
+			doorChoice = input.nextInt();
+			
+			if(doorChoice == 1)
+			{
+			result = "pile of gold";
+			}
+			else if(doorChoice == 2)
+			{
+			result = "I'm a donkey";
+			}
+			else if(doorChoice == 3)
+			{
+			result = "Batman";
+			}
+			else 
+			{
+				result = "nothing because thar was not a choice!";
+			}
+			displayMessage(result);
+	}
+public static void displayMessage (String result) {
+		System.out.println("You won : " + result);
+	
+}
+}
