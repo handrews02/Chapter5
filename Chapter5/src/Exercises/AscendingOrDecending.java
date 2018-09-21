554d2a10d8564e0374336263316ecda23bba4934
Hannah Andrews <handrews02@users.noreply.github.com>
2018-09-21 09:51:21 -0500

    updated text

---
+++ Chapter5/src/Exercises/AscendingOrDecending.java
@@ -0,0 +1,24 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class AscendingOrDecending {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1;
+		int num2;
+		int num3;
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Please enter a number >>");
+		num1 = input.nextInt();
+		System.out.println("Please enter another number >>");
+		num2 = input.nextInt();
+		System.out.println("Please enter a final number >>");
+		num3 = input.nextInt();
+		
+	}
+
+}
