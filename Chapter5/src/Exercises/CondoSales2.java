fcb1a2d00ec2133802c75e0f3803a0b9b68a5687
Hannah Andrews <handrews02@users.noreply.github.com>
2018-09-25 09:09:15 -0500

    updated text

--- Chapter5/src/Exercises/CondoSales2.java
+++ Chapter5/src/Exercises/CondoSales2.java
@@ -2,7 +2,7 @@ package Exercises;
 
 import java.util.Scanner;
 
-public class CondoSales {
+public class CondoSales2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
